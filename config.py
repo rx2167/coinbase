3eaa5627e0a0673a271469e0476d7f77484bf4e4
renGz <54996197+rx2167@users.noreply.github.com>
2022-02-24 16:19:18 -0500

    coinbase api

---
+++ config.py
@@ -0,0 +1,10 @@
+client_id = 'R1CFVAQCQYYZV0DSDANIFXQAFTKVXAGH@AMER.OAUTHAP'
+token_path = 'token'
+redirect_uri = 'http://localhost'
+
+account_id = 489947028
+
+# put path to chromedriver here
+chromedriver_path='/Users/larry/Projects/tdameritrade/chromedriver'
+
+gasp hawk problem never feature gasp okay smart nerve mass arena lobster
---
+++ configs.py
@@ -0,0 +1,5 @@
+api_secret = "x+zajwcASNpyIZUAPYUbDCzvyxXgz7xf5kRxseFLwdRhO1f4NXshoddKheMRSomH7W0dJUjGlbtt5pxAORYgRg=="
+api_key	= "cd66ce5a639f411d9052866646919394"
+api_pass = "notSandbox"
+
+
